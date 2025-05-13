505cfc6c00ed5e63f2798728a91efc120a81f6ab
aleksandregakharia <gakhariaalexs@gmail.com>
2025-05-12 19:38:08 -0700

    second commit

--- Level 06/homework/index.py
+++ Level 06/homework/index.py
@@ -1,14 +1,1 @@
-# რას გამოიტანს ეს კოდი?
-x = 10
-print(x > 5 and x < 15) 
-# True
-
-
-age = 17
-print(age < 18 or age > 65)
-# true
-
-
-password = "admin"
-print(password != "1234" and password != "password")
-# true
+# davaleba ar gvaqvs
