402901861e56bbc7e7514252ea694dcf9f2f9f08
aleksandregakharia <gakhariaalexs@gmail.com>
2025-05-17 00:04:47 -0700

    second commit

---
+++ Level 08/classwork/index.py
@@ -0,0 +1,5 @@
+# <!-- controlflow aris programis shignit kodis shesrulebis tanmimdevroba -->
+
+
+for i in range(10) :
+   print("hello" , i )
---
+++ Level 08/homework/index.py
@@ -0,0 +1,23 @@
+for i in range(5) :
+   print("hello world" , i )
+
+
+# algoritmi aris nabij nabij instruqcia
+# flowchart algoritmis naxatad warmodgena
+# pseudocode iyeneben algoritmis psevdo kodad anu zogjer mis magivrad iyeneben
+
+
+
+
+
+
+for i in range(10) :
+    print("qartuli yvela kaia" , i)
+
+
+for i in range(100) :
+    print("sandwich" , i)
+
+
+for i in range(100) :
+   print(i)
