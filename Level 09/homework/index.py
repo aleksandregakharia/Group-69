d4f7243403139832b7ce9e62f2d0e5428f560d98
aleksandregakharia <gakhariaalexs@gmail.com>
2025-05-22 16:05:23 -0700

    second commit

---
+++ Level 09/homework/index.py
@@ -0,0 +1,30 @@
+# 2) მოიყვანეთ while loop-ის მაგალითი და კომენტარის სახით ახსენით, თუ როგორ მუშაობს while ციკლი;
+# while loop aris martivad ro avxsnat ragac rac tavis saqmes iqmade aketebs sanam martalia
+
+
+i = 8
+while i < 19  :
+   print(i + 1)
+   i = i + 1
+
+
+# 3) კომენტარის სახით ახსენით თუ რა განსხვავებაა for loop-სა და while loop-ს შორის 
+# while loops ar aqvs range da iqmade grdzeldeba sanam martalia xolo for loops aqvs range
+
+
+# 4)გამოიტანეთ თქვენი სახელი და გვარი იმდენჯერ, რამდენი წლისაც ხართ 
+
+
+a = 0
+while a < 15 :
+   print('alex gakharia')
+   a = a + 1
+
+
+# 5)გამოიტანეთ რიცხვები 10-დან 15-მდე 
+
+
+l = 10
+while l <= 15 :
+   print(l)
+   l = l + 1
