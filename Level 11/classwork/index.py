a7199793766ab3bd4133f60a6ee76167710cb8ee
aleksandregakharia <gakhariaalexs@gmail.com>
2025-06-03 00:09:40 -0700

    second commit

---
+++ Level 11/classwork/index.py
@@ -0,0 +1,42 @@
+# 1)დაბეჭდე რიცხვები 1-დან 10-მდე.
+for i in range(11) :
+    print(i)
+
+# 2
+l= 10
+for l in range(10, 0 , 1) :
+     print(l)
+
+
+# 6)დაბეჭდე რიცხვები 1-დან 10-მდე.
+i = 0
+while i < 10 :
+    print(i + 1)
+    i = i + 1
+
+# 7)დაბეჭდე ყველა ლუწი რიცხვი 1-დან 20-მდე.
+a= 2 
+while a <= 20:
+    print(a)
+    a += 2 
+
+#10)დაბეჭდე რიცხვები 10-დან 1-მდე უკუღმა.
+
+n = 11
+while n > 0 :
+    print(n - 1)
+    n = n - 1
+
+# 9)მომხმარებელს შეჰყავს რიცხვი n. დაბეჭდე 1-დან n-მდე რიცხვების ჯამი.
+
+N_ricxvi = int(input("sheiyavne ricxvi"))
+for N_ricxvi in range(N_ricxvi + 1) :
+    print
+
+
+# 
+ik = 0
+for i in range (1 , 101 , 2) :
+   ik += i
+
+
---
+++ Level 11/homework/index.py
@@ -0,0 +1,31 @@
+# 2)ახსენით კომენტარებით while loop და for loop განსხვავება 
+# while loopi grdzeldeba iqamde sanam True aris da ar vicit zustad ramdenjer gvinda ganmeoreba
+# xolo for loops viyenebt rodesac zustad vicit ramdenjerac unda ganmeordes
+
+
+# 3)მომხმარებელს შემოატანინეთ რიცხვი და ეს რიცხვი თუ იყო 18 მეტი      დაპრინტეთ "you are big adult" "if ით გააკეთეთ"
+age = int(input("sheiyvane shemi asaki"))
+if age > 18 :
+    print("you are big adult")
+
+
+# 4)გააკეთეთ 3 დავალება while loop ით 
+i = 0
+while i < 10 :
+    print("hello")
+    i = i + 1
+
+
+a = 70
+while a < 100 :
+    print(a)
+    a = a + 2
+
+
+l = 0 
+while l > 100:
+    print("salami")
+    l = l + a
+
+
+# sololearn
