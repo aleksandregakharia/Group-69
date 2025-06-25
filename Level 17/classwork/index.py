3ca591a166fbbe7b478356bd126dfd3a41962b33
aleksandregakharia <gakhariaalexs@gmail.com>
2025-06-24 18:09:02 -0700

    second commit

---
+++ Level 17/classwork/index.py
@@ -0,0 +1,24 @@
+# 1)შექმენი სია სადაც თქვენი საყვარელი საწმელები იქნeba
+
+s = ["khinkali", "toast", "khawapuri", "lobiani"]
+
+
+# 2)შექმენით ახალი სია სადაც იქნება color ცვლადი და ცისარტყელის ფერები იქნება ცხრილში და მხოლოდ ცივ ფერებს გამოიტანეთ 
+
+c = ["color" , "witeli", "yviteli", "lurji", "iasamnisferi", "vardisferi","mwvane", ]
+print(c [4], c [5])
+
+#  3)შექმენით სია სადაც 5 ცხოველი იქნება და მხოლოდ 1 და 2 გამოიტანთ
+a =["lomi", "mgelli","zvigeni", "dzagli", "kata"]
+print(a[1], a[2])
+
+# 4)შექმენით 2 სია და  1 სიიდან 2 ინდექს მიუმატებ 2 სიის 3 ინდექს ჯამი დაპრინტეთ
+
+p = [1,2,3,4,5]
+d = [2,3,4,5,6,]
+
+print(p[2] - d[3])
+
+
+# 5)კომენტარების მეშვეობით ახსენი როგორ გამოვიყენოთ სია
+# sia rom gamoviyenot jer unda shevqnmant cvlaid shemdeg [da amatSi chavwerot siis monacemebi]
---
+++ Level 17/homework/index.py
@@ -0,0 +1,16 @@
+# 1)შექმენი სია შენი 5 საყვარელი ხილით და დაბეჭდე ეს სია.
+
+a = ["bali", "banani", "sazamtro", "atami", "vashli"]
+
+
+# 2)ახსენით რითი განსხვავდება ცვლადი list ისგან 
+# cvladshi shevinaxavt mxolod ert girebulebas xolo listshi usasrulot
+
+
+# 3)უყურეთ დღევანდელ ჩანაწერს 
+#  4)შექმენი სია სადაც იქნება ტოლმების სახეობა და 4 ინდექსძე და 2 ინდექსზე მყოფი ტოლმები შეაერთე 
+# 5)sololearn
+
+
+t = ["ixvis", "qatmis", "dzroxis", "goris", "cxenis"]
+print(t[2] + t[4])
