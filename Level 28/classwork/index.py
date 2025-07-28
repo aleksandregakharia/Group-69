f47107cc42b0ffcaf02df5c2dff26467ed1859a4
aleksandregakharia <gakhariaalexs@gmail.com>
2025-07-28 07:28:12 -0700

    second commit

---
+++ Level 28/classwork/index.py
@@ -0,0 +1,52 @@
+name = "alexs"
+print(name.upper())
+
+name2 = "ALEKSANDRE"
+print(name2.lower())
+
+
+nam3 = "nuca"
+print(nam3.capitalize())
+
+
+text = ("hello")
+print(text.find("l"))
+
+
+# 1) ახსენით რისთვის გამოიყენება upper(), lower(), capitalize(), find()
+# upper() gaqvset tekstis yvela asoa didi asoead gadaketebis
+# lower() tekstis yvela asos patara asodad gadaketeba
+# capitalize() tekstis pirveli aso didi asot aqcevs sxvebs apataravebs
+# find() edzebs textshi konkretul mibolo meramdene index ia
+
+# 2) რისი გაკეთება შეგვიძლია დოტ ნოტაციის მეშვეობით?
+# Dot notation – es aris . simbolo romlitac printis shignit shignit vwert da gamoviyenebt ragac funqciass
+
+
+numbers = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+print(len(numbers))
+
+
+num = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9]
+num.append(10)
+print(num)
+
+
+G = [1 , 2 , 3 , 4 , 5]
+G.insert(2,"nuca")
+print(G)
+
+
+N = [ 1 , 2 , 3 , 4]
+N.pop(3)
+print(N)
+
+
+# 3)  ახსენით რისთვის გამოიყენება len(), append(), insert(), pop().
+# len gvihvenebs ramdeni elementia listshi , append boloshi amatebs mititebul mnishvnelobas, insert chaanacvlebs mititebul index
+# pop amoagdebs mititebul index s list idan
+
+
+# 4) ახსენით რა განსხავებაა მეთოდსა და ჩაშენებული-ფუნქციის შორის
+# mat shoris aris ganxvaveba ert erti tviton gveubneba rom aris chashenebuli da iwereba . lis gareshe pirdapir
+# xolo meores wirdeba .tili da mis shemdeg vidzaxebt funqcias
