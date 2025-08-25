783042c7fb3a66c4f6acf48599c202eeea1b089c
aleksandregakharia <gakhariaalexs@gmail.com>
2025-08-25 10:30:34 -0700

    second commit

---
+++ Level 36/classwork/index.py
@@ -0,0 +1,15 @@
+a = "abc"
+
+b = "b"
+
+for i in a :
+    if i in b :
+        print(i)
+
+
+
+# 1) ახსენით რაში გამოიყენება not და in
+# in - gamoiyeneba imistvis, rom shevamowmot, aris tu ara ragac elementi ragaca shi.
+# Es sheidzleba iyos sia, teqsti, monacemta baza da ase shemdeg.
+# not nishnavs uarist tqmnas.
+# Tu ragac aris True, mashin not gaketebs misi uars – False. Da pirikit.
---
+++ Level 36/homework/index.py
@@ -0,0 +1,26 @@
+# 1) ახსენით რა განსხვავებაა return-სა და print შორის
+# print shi shegvidzlia davwerot rac gvinda da roca gvinda xolo return shi shegvidzlia gamoviyrnot funqciashi da inaxavs mnishvnelobas
+
+
+# 2) ახსენით როგორ იქმნება def ფუნქცია
+# jer vwer def s shemdeg vqnit funqciis saxels da masshi vwert argument tu gvinda xolo bolos returnshi vwert rasac moitxovs
+
+
+# 3) ახსენით რას ნიშნავს keyword-ი, და ჩამოთავლეთ რამდენიმე
+# Keyword aris spetsialuri sityva romelic Python-shi ukve gamoyenebulia da dakavebulia gansazRvruli mnishvnelobit mas vegar gamoviyenebt chvens nebaze
+# False
+# True
+# and
+# def
+# elif
+# else
+# for
+# from
+# if
+# in
+# not
+# or
+# return
+# while
+
+
